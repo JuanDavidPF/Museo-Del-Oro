14c6bafadcc94fcd23dd146d73d39f0976776d86
JuanDavidPF <JuanFavidPF@gmail.com>
2019-11-18 02:30:30 -0500

    added some path fixes

---
+++ bin/sripts/quienEres.js
@@ -0,0 +1,10 @@
+let perfil = document.querySelectorAll(".perfil")
+
+for (let index = 0; index < perfil.length; index++) {
+
+    perfil[index].addEventListener("click", function () {
+
+
+    })
+
+}
