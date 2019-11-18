b29fe633274db0c1bd3fc0d43839f37b3f0001b0
JuanDavidPF <JuanFavidPF@gmail.com>
2019-11-18 02:49:15 -0500

    ADded some polishment

--- bin/sripts/quienEres.js
+++ bin/sripts/quienEres.js
@@ -5,6 +5,21 @@ for (let index = 0; index < perfil.length; index++) {
     perfil[index].addEventListener("click", function () {
 
 
+
+        for (let j = 0; j < perfil.length; j++) {
+
+            if (perfil[j] == perfil[index]) {
+                perfil[j].style.transition = "border-width 0.3s ease-in-out"
+                perfil[j].style.borderWidth = "7px"
+             
+            } else {
+                perfil[j].style.borderWidth = "0"
+            }
+
+        }
+
+
+
     })
 
 }
